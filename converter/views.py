92c0bcc976a9b4dde0d00dfd2fed7df0551ac9aa
larry852 <larryportocarrero@gmail.com>
2017-11-29 12:14:51 -0500

    Integracion con google cloud

--- converter/views.py
+++ converter/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from wand.image import Image
 import base64
+from google.cloud import vision
 
 
 def index(request):
@@ -9,7 +10,11 @@ def index(request):
         pdf = request.FILES['pdf']
         fs = FileSystemStorage()
         filename = fs.save('input/' + pdf.name, pdf)
+        # Convert pdf to image
         pages = convert(filename)
+        # Get OCR
+        for page in range(pages):
+            extractOCR('page-{}.jpg'.format(page))
 
         return render(request, 'upload.html', {
             'pages': range(pages)
@@ -31,3 +36,16 @@ def getBase64(filename):
     with open('media/output/' + filename, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     return encoded_string
+
+
+def extractOCR(filename):
+    """Detects text in the file."""
+    client = vision.ImageAnnotatorClient()
+    with open('media/output/' + filename, 'rb') as image_file:
+        content = image_file.read()
+    image = vision.types.Image(content=content)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+    for text in texts:
+        print('\n"{}"'.format(text.description))
