612958414903ca7a2413ba9cd4459d355faef94d
larry852 <larryportocarrero@gmail.com>
2017-11-29 11:41:17 -0500

    Metodo parametrizable de funcion de conversion

--- converter/views.py
+++ converter/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from wand.image import Image
+import base64
 
 
 def index(request):
@@ -8,13 +9,25 @@ def index(request):
         pdf = request.FILES['pdf']
         fs = FileSystemStorage()
         filename = fs.save('input/' + pdf.name, pdf)
-        # Converting pdf to JPG
-        with Image(filename='media/' + filename, resolution=300) as img:
-            img.save(filename="media/output/page.jpg")
-            pages = len(img.sequence)
+        pages = convert(filename)
 
         return render(request, 'upload.html', {
             'pages': range(pages)
         })
 
     return render(request, 'upload.html')
+
+
+def convert(filename, resolution=300):
+    pages = 0
+    with Image(filename='media/' + filename, resolution=resolution) as img:
+        img.save(filename="media/output/page.jpg")
+        pages = len(img.sequence)
+    return pages
+
+
+def getBase64(filename):
+    encoded_string = ""
+    with open('media/output/' + filename, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    return encoded_string
