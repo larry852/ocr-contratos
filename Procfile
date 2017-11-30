6ae3fa76c242cfc13fe9d7c92971cb7f9f883176
larry852 <larryportocarrero@gmail.com>
2017-11-29 21:59:37 -0500

    Configuracion inicial Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn contratos.wsgi --log-file -
--- contratos/settings.py
+++ contratos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,8 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
