38afaee70fd39c681aa18f5b5c3fc6c3140fb17a
larry852 <larryportocarrero@gmail.com>
2017-12-01 18:56:05 -0500

    FieldFile para archivo

--- converter/models.py
+++ converter/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Document(models.Model):
     nit = models.CharField(max_length=15, default='No detect')
-    file = models.ImageField(upload_to='input')
+    file = models.FileField(upload_to='input')
     uploaded_at = models.DateTimeField(auto_now_add=True)
