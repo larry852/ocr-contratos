fa56ae7356c8719bd2688ac6c04c0741592bfb40
larry852 <larryportocarrero@gmail.com>
2017-12-01 21:50:46 -0500

    Limitando acciones de admin panel

--- converter/admin.py
+++ converter/admin.py
@@ -6,7 +6,12 @@ from .models import Document
 @admin.register(Document)
 class AdminDocument(admin.ModelAdmin):
     list_display = ('nit', 'document', 'uploaded_at')
+    actions = ['delete']
+    list_display_links = None
     ordering = ('-uploaded_at',)
+
+    def has_add_permission(self, request):
+        return False
 
     def document(self, obj):
         name = obj.file.name.replace("input/", "").replace(".pdf", "")
