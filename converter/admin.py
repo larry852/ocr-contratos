3bc13853e70db10cd8045c8268a2bc031b892622
larry852 <larryportocarrero@gmail.com>
2017-12-01 20:39:44 -0500

    Visualizacion admin django

--- converter/admin.py
+++ converter/admin.py
@@ -5,4 +5,14 @@ from .models import Document
 
 @admin.register(Document)
 class AdminDocument(admin.ModelAdmin):
-    list_display = ('nit', 'file', 'uploaded_at')
+    list_display = ('nit', 'document', 'uploaded_at')
+
+    def name(self, obj):
+        return obj.file.name
+
+    def document(self, obj):
+        name = obj.file.name.replace("input/", "").replace(".pdf", "")
+        return """<a href={}> {} </a>""".format(obj.file.url, name)
+
+    document.short_description = 'document'
+    document.allow_tags = True
